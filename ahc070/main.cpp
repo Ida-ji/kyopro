2097bb0c5aef6d978cbdaedcb103f52d27f7b6e5
Ida-ji <ikeikeda301@gmail.com>
2026-08-29 12:13:37 +0900

    要らないファイルを消した

---
+++ ahc070/main.cpp
@@ -0,0 +1,14 @@
+//This code uses templates generated with AI before the contest.
+//Repository URL : https://github.com/Ida-ji/kyopro/tree/main/template
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using vi = vector<int>;
+using vb = vector<bool>;
+using vvi = vector<vector<int>>;
+using vvb = vector<vector<bool>>;
+using pii = pair<int, int>;
+using vpii = vector<pair<int, int>>;
+
+#define rep(i, s, t) for (int i = s; i < t; i++)
